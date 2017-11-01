55e9972cfbb1174de7b633622e63cc02dba08d81
Yuriy Stetskyi <ystetskyy333@gmail.com>
2017-11-01 21:42:24 +0200

    Created Register window

---
+++ ListOfCosts/Models/Helpers/RegisterBindingHelper.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListOfCosts.Models.Helpers
+{
+    public class RegisterBindingHelper : INotifyPropertyChanged
+    {
+        public RegisterBindingModel Model { get; set; }
+
+        public RegisterBindingHelper()
+            : this(new RegisterBindingModel())
+        {
+        }
+
+        public RegisterBindingHelper(RegisterBindingModel _m)
+        {
+            Model = _m;
+
+            LoginValidation = "";
+            PasswordValidation = "";
+            ConfirmPasswordValidation = "";
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void OnPropertyChanged(string propertyName)
+        {
+            if(PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+        public bool Validate()
+        {
+            return ValidateLogin() & ValidatePassword() & ValidateConfirmPassword();
+        }
+
+
+        #region Validation Properties
+
+        private string loginValidation;
+
+        private string passwordValidation;
+
+        private string confirmPasswordValidation;
+
+        public string LoginValidation
+        {
+            get
+            {
+                return loginValidation;
+            }
+            set
+            {
+                if(loginValidation != value)
+                {
+                    loginValidation = value;
+                    OnPropertyChanged("LoginValidation");
+                }
+            }
+        }
+
+        public string PasswordValidation
+        {
+            get
+            {
+                return passwordValidation;
+            }
+            set
+            {
+                if (passwordValidation != value)
+                {
+                    passwordValidation = value;
+                    OnPropertyChanged("PasswordValidation");
+                }
+            }
+        }
+
+        public string ConfirmPasswordValidation
+        {
+            get
+            {
+                return confirmPasswordValidation;
+            }
+            set
+            {
+                if (confirmPasswordValidation != value)
+                {
+                    confirmPasswordValidation = value;
+                    OnPropertyChanged("ConfirmPasswordValidation");
+                }
+            }
+        }
+
+        #endregion
+
+        #region Validation Methods
+
+        private bool ValidateLogin()
+        {
+            if (string.IsNullOrWhiteSpace(Model.Login))
+            {
+                LoginValidation = "Login cannot be empty";
+                return false;
+            }
+            else
+            {
+                LoginValidation = "";
+                return true;
+            }
+        }
+
+        private bool ValidatePassword()
+        {
+            if (string.IsNullOrWhiteSpace(Model.Password))
+            {
+                PasswordValidation = "Password cannot be empty";
+                return false;
+            }
+            else
+            {
+                PasswordValidation = "";
+                return true;
+            }
+        }
+
+        private bool ValidateConfirmPassword()
+        {
+            if (string.IsNullOrWhiteSpace(Model.ConfirmPassword))
+            {
+                ConfirmPasswordValidation = "Password cannot be empty";
+                return false;
+            }
+            else if(Model.ConfirmPassword != Model.Password)
+            {
+                ConfirmPasswordValidation = "Passwords do not match";
+                return false;
+            }
+            else
+            {
+                ConfirmPasswordValidation = "";
+                return true;
+            }
+        }
+
+        #endregion
+
+    }
+}
---
+++ ListOfCosts/Register.xaml.cs
@@ -0,0 +1,66 @@
+﻿using ListOfCosts.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ListOfCosts
+{
+    /// <summary>
+    /// Interaction logic for Register.xaml
+    /// </summary>
+    public partial class Register : Window
+    {
+        public Register()
+        {
+            InitializeComponent();
+            DataContext = new RegisterViewModel();
+        }
+
+        private void passwordBox_PasswordChanged(object sender, RoutedEventArgs e)
+        {
+            var dc = DataContext as RegisterViewModel;
+            dc.SetPassword((sender as PasswordBox).Password);
+        }
+
+        private void confirmPasswordBox_PasswordChanged(object sender, RoutedEventArgs e)
+        {
+            var dc = DataContext as RegisterViewModel;
+            dc.SetConfirmPassword((sender as PasswordBox).Password);
+        }
+
+        private void register_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                var dc = DataContext as RegisterViewModel;
+
+                if (dc.Validate())
+                {
+                    dc.Register();
+                    MessageBox.Show("New user has been successfully registered");
+                }
+            }
+            catch(Exception exc)
+            {
+                MessageBox.Show(exc.Message);
+            }
+        }
+
+        private void back_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow w = new MainWindow();
+            w.Show();
+            this.Close();
+        }
+    }
+}
